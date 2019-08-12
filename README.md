f2b452173e1e89b8e4227855ffd121b9fafaffce
13abel <therichardwei@gmail.com>
2019-08-11 21:00:20 -0400

    added init script and moved config outside of home folder

---
+++ README.md
@@ -0,0 +1,5 @@
+# uwu-files
+
+contains my dot files and config files for various programs
+
+instructions and inspo from [here](https://www.freecodecamp.org/news/dive-into-dotfiles-part-2-6321b4a73608)
---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# creates symlinks for dot files and copies them over to the appropriate locations
+
+installSymLinks() {
+    local homeFiles=($(ls -A "home"))
+    for file in "${homeFiles[@]}"; do
+        ln -svf "home/$file" "$HOME"
+    done
+
+    local configFiles=($(ls -A ".config"))
+    for file in "${configFiles[@]}"; do
+        ln -svf ".config/$file" "$HOME/.config/"
+    done
+}
+
+installSymLinks
