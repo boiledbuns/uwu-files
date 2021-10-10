b34af51bb4975ec89fa611260ede68d5807db792
richard wei <therichardwei@gmail.com>
2021-10-10 15:09:49 -0400

    added emacs config

--- init_mac.sh
+++ init_mac.sh
@@ -13,6 +13,13 @@ installSymLinks() {
         echo $file
         ln -svf "$PWD/.config/$file" "$HOME/.config/"
     done
+
+    # emacs 
+    echo "emacs config files:"
+    local emacsConfig=($(ls -A "emacs"))
+    for file in "${emacsConfig[@]}"; do
+        ln -svf "$PWD/emacs/$file" "$HOME/.emacs.d/"
+    done
 }
 
 installSymLinks
